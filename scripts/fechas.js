d1eae5cc41beb2fd1320df29b373f130f82e8d21
Dario Perez <darioperez.2105@gmail.com>
2023-02-08 18:19:04 -0300

    Correcting dates exersice

--- scripts/fechas.js
+++ scripts/fechas.js
@@ -1,10 +1,9 @@
 const currentDate = new Date();
 
 const myBirthday = new Date(1988, 5, 21);
-console.log(myBirthday);
+
+const compar = currentDate > myBirthday;
 
 const dayOfBirth = myBirthday.getDate();
 const monthOfBirth = myBirthday.getMonth() + 1;
 const yearOfBirth = myBirthday.getFullYear();
-
-console.log(dayOfBirth, monthOfBirth, yearOfBirth);
